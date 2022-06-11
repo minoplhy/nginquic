4330c3c9ef7b929a020ecd19a280c27f71760d9d
unknown <hej@aspoblourib.1w1.one>
2022-06-11 20:09:10 +0700

    nginquic : moved from scriptbox

---
+++ build.sh
@@ -0,0 +1,18 @@
+cd ~/
+rm -rf nginquic
+curl -sSL https://raw.githubusercontent.com/minoplhy/nginquic/main/packages.sh | bash
+mkdir nginquic && cd nginquic
+hg clone -b quic https://hg.nginx.org/nginx-quic
+git clone https://github.com/google/boringssl
+cd boringssl
+mkdir build && cd build && cmake .. && make
+cd .. && cd ..
+cd nginx-quic
+mkdir mosc && cd mosc && curl -sSL https://raw.githubusercontent.com/minoplhy/nginquic/main/modules.sh | bash && cd ..
+curl -sSL https://raw.githubusercontent.com/minoplhy/nginquic/main/configure.sh | bash && make
+mkdir /lib/nginx/ && mkdir /lib/nginx/modules
+cd objs && cp *.so /lib/nginx/modules
+rm /usr/sbin/nginx
+cp nginx /usr/sbin/nginx
+curl -sSL https://raw.githubusercontent.com/minoplhy/nginquic/main/modules.conf > modules.conf
+cp modules.conf /etc/nginx/modules-enabled
