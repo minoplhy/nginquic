9b36007276bb74ba9e548ac18111719844b3f483
unknown <hej@aspoblourib.1w1.one>
2022-11-29 22:11:53 +0700

    package.sh : use g++ instead of c++

--- packages.sh
+++ packages.sh
@@ -1,1 +1,1 @@
-sudo apt-get install mercurial libunwind-dev libpcre3 libpcre3-dev zlib1g-dev cmake make libxslt1-dev libgd-dev libssl-dev libperl-dev libpam0g-dev libgeoip-dev git c++ -y
+sudo apt-get install mercurial libunwind-dev libpcre3 libpcre3-dev zlib1g-dev cmake make libxslt1-dev libgd-dev libssl-dev libperl-dev libpam0g-dev libgeoip-dev git g++ -y
