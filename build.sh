1d25a25e4211efc0bcb826815ee31fc20bb4287c
unknown <hej@aspoblourib.1w1.one>
2022-11-29 22:08:09 +0700

    build.sh : Download Golang via Source from now on

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@ cd ~/
 rm -rf nginquic
 curl -sSL https://raw.githubusercontent.com/minoplhy/nginquic/main/packages.sh | bash
 mkdir nginquic && cd nginquic
+
+# Install Golang
+unlink /usr/bin/go
+wget https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+ln -s /usr/local/go/bin /usr/bin/go
+
 hg clone -b quic https://hg.nginx.org/nginx-quic
 git clone --depth=1 https://github.com/google/boringssl
 cd boringssl
